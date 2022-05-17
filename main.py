64d9640b2b24d95daef8410b2b96c6bd0e615c15
Transa152900 <76061884+Transa152900@users.noreply.github.com>
2022-05-17 15:02:00 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+client=commands.Bot(command_prefix='!', status=discord.Status.idle)
+
+@client.event
+async def on_ready():
+	print('Started')
+
+client.run(os.environ["DISCORD_TOKEN"], bot=False)
